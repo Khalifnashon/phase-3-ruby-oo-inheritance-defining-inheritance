96d03033008de96fd7dbe9e25e62b237d79f5ab8
+ <+>
2023-02-15 10:51:19 +0300

    completed code along

--- lib/car.rb
+++ lib/car.rb
@@ -1,2 +1,19 @@
 # This loads the code from the vehicle.rb file so we can access its code within this file
 require_relative './vehicle'
+
+# We use the < to inherit the Car class from Vehicle. 
+# Notice that there are no methods defined in the Car class.
+class Car < Vehicle
+    # Overwriting a method in the Vehicle class.
+    def go
+        "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+    end
+end
+
+# Open up IRB, and start by requiring the files from the lib folder:
+
+# require_relative 'lib/vehicle'
+# => true
+# require_relative 'lib/car'
+# => true
+# This will let you interact with the code you've written in those files from within IRB.
--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,3 +1,22 @@
+require "pry"
+# Instances of Vehicle initialize with a wheel size and number. We also have
+#  go and #fill_up_tank instance methods that describe some common vehicle behavior.
 class Vehicle
 
+    attr_accessor :wheel_size, :wheel_number
+  
+    def initialize(wheel_size, wheel_number)
+      @wheel_size = wheel_size
+      @wheel_number = wheel_number
+    end
+  
+    def go
+      "vrrrrrrrooom!"
+    end
+  
+    def fill_up_tank
+      "filling up!"
+    end
+  
 end
+binding.pry
